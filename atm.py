0aee4bc346a768c34db12679f4a4e75db83b009d
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2021-01-15 14:56:58 +0530

    commit

--- atm.py
+++ atm.py
@@ -1,27 +1,27 @@
 print("Welcome to the fastest Banking System!")
 ac_num = int(input("Enter your Registed Bank Account Number"))
 pin_num = int(input("Enter your pin number"))
-def choose(i):
-    switcher={
-        1:balance(),
-        2:deposit(),
-        3:withdraw()
-    }
-    return switcher.get(i,"Invalid input")
-def balance():
-    print("balance is 25000")
-def deposit():
-    print("hey")
-def withdraw():
-    print("good")
-if (ac_num == 282828) : 
-    if( pin_num == 2222):
+balance = 25000
+if (ac_num == 2) : 
+    if( pin_num == 8):
         print("Welcome, Please choose your transaction of your choice: ")
         print("1.Balance Enquiry")
         print("2.Deposit")
         print("3.Withdrawl")
         choice =int(input("We are waiting for your options.... "))
-        choose(cho)
+        if(choice == 1):
+            print("Balance is:",balance)
+        elif(choice == 2):
+            deposit = int(input("Enter amount you want to deposit: "))
+            balance = balance+deposit
+            print("Balance is:",balance)
+        elif(choice == 3):
+            withdraw = int(input("Enter amount you want to deposit: "))
+            balance = balance-withdraw
+            print("Balance is:",balance)
+        else:
+            print("Invalid Input!")            
+
     else:
         print("Wrong pin number!")
 
